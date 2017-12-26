81cde7897d609af2fee44ef96b85a6a31643ec80
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-12-26 23:49:56 +0000

    Add game playing state

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
 init();
 
@@ -17,33 +17,39 @@ var diceDom = document.querySelector('.dice');
 diceDom.style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
-    var dice = Math.floor(Math.random() * 6) + 1;
+    if (gamePlaying) {
+        var dice = Math.floor(Math.random() * 6) + 1;
 
     
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+        diceDom.style.display = 'block';
+        diceDom.src = 'dice-' + dice + '.png';
 
-    if (dice !== 1) {
-        roundScore += dice;
-        document.querySelector("#current-" + activePlayer).textContent = roundScore;
-    } else {
-        nextPlayer();
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector("#current-" + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
     }
     
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    scores[activePlayer] += roundScore;
+    if (gamePlaying) {
+        scores[activePlayer] += roundScore;
 
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        diceDom.style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-    } else {
-        nextPlayer();
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            diceDom.style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
     }
 });
 
@@ -66,6 +72,8 @@ function init() {
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
+
+    gamePlaying = true;
 
     document.getElementById("score-0").textContent = 0;
     document.getElementById("current-0").textContent = 0;
