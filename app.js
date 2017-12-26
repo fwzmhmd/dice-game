2ef6ccd3a9c95286ca5ac6446978b695644c4cdb
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-12-26 23:29:18 +0000

    Add function to start the game

--- app.js
+++ app.js
@@ -11,14 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.getElementById("score-0").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-1").textContent = 0;
+init();
 
 var diceDom = document.querySelector('.dice');
 diceDom.style.display = 'none';
@@ -65,4 +58,29 @@ function nextPlayer() {
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.getElementById("score-0").textContent = 0;
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    
 }
