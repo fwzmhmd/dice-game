34a6e46f3b819e0bfbbba76a1f1fe9bd424a2556
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-12-27 00:15:33 +0000

    Change winning score to 100

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             diceDom.style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
